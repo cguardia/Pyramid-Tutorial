563b69402ad0aebf677dc9d2141921a6d5ed8130
Carlos de la Guardia <cguardia@yahoo.com>
2011-07-13 11:43:30 -0500

    changed theme to pyramid

--- conf.py
+++ conf.py
@@ -91,7 +91,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+html_theme = 'pyramid'
+
+html_theme_options = {
+    'github_url': 'https://github.com/Pylons/pyramid'
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
