6db5da6480d70ab5ac2345ec862f2c18040524b1
Chris McDonough <chrism@plope.com>
2012-02-12 19:00:53 -0500

    remove unused imports

--- src/stage3/birdie/models.py
+++ src/stage3/birdie/models.py
@@ -1,19 +1,14 @@
-import transaction
-
 from sqlalchemy import Column
 from sqlalchemy import TIMESTAMP
 from sqlalchemy import Unicode
 from sqlalchemy import Integer
 from sqlalchemy import ForeignKey
 
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import column_property
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import backref
 from zope.sqlalchemy import ZopeTransactionExtension
 
 from pyramid.security import Authenticated
