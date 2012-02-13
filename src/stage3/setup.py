9faa144775c670818aef963bec22778e777e2054
Chris McDonough <chrism@plope.com>
2012-02-12 19:10:01 -0500

    depend on pyramid_tm, pyramid_debugtoolbar, waitress, remove dependency on weberror, transaction, and repoze.tm2; pyramid 1.3 no longer supports 2.4 or 2.5, so remove pysqlite addition conditiona;

--- src/stage3/setup.py
+++ src/stage3/setup.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from setuptools import setup, find_packages
 
@@ -10,16 +9,13 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid',
     'SQLAlchemy',
-    'transaction',
-    'repoze.tm2>=1.0b1', # default_commit_veto
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
     'zope.sqlalchemy',
     'repoze.timeago',
     'cryptacular',
-    'WebError',
+    'waitress',
     ]
-
-if sys.version_info[:3] < (2,5,0):
-    requires.append('pysqlite')
 
 setup(name='birdie',
       version='0.0',
